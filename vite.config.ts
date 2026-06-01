3c2c109350fc16390b884a508bae78cfcee90e6d
maheshstartrader <mahesh.startrader@gmail.com>
2026-06-01 17:12:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   plugins: [],
+
   tanstackStart: {
-    server: false,
+    server: {
+      preset: "cloudflare",
+    },
   },
 });
